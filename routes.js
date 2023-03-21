df37ba8b4c2837f03c3cc5fcedef08d424ee2bf7
Rizkiocu <rizkiprasetia98@gmail.com>
2023-03-21 14:15:22 +0700

    revisi kedua

--- routes.js
+++ routes.js
@@ -18,7 +18,7 @@ const routes = [
         method : '*',
         path : '/',
         handler : (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
+            return 'Halaman tidak dapat diakses dengan method tersebut!';
         }
     },
     {
